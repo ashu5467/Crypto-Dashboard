9099936b76f883e7478ccf46e2edc2d91cb001b3
Ashutosh Gawali <ashug5467@gmail.com>
2022-11-19 22:57:21 +0530

    first commit

---
+++ src/components/Button/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+function Button({ text, onClick }) {
+  return (
+    <div className="button-wrapper" onClick={onClick}>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default Button;
+
+
